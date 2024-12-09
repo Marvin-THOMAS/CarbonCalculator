ea25a455f5eec342f9ece1ee52127a104c316d66
Marvin THOMAS <marvin.thomas2023@campus-eni.fr>
2024-12-09 16:54:12 +0100

    tpcarbon-foot-print

--- src/app/components/carbon-footprint/carbon-footprint.component.ts
+++ src/app/components/carbon-footprint/carbon-footprint.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import {CarbonFootprintFormComponent} from "../carbon-footprint-form/carbon-footprint-form.component";
 import {CarbonFootprintResultComponent} from "../carbon-footprint-result/carbon-footprint-result.component";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-carbon-footprint',
   standalone: true,
   imports: [
     CarbonFootprintFormComponent,
-    CarbonFootprintResultComponent
+    CarbonFootprintResultComponent,
+    NgClass
   ],
   templateUrl: './carbon-footprint.component.html',
   styleUrl: './carbon-footprint.component.css'
 })
 export class CarbonFootprintComponent {
+
+
+  public distanceKm : number = 501;
+  public consommationPour100Km :  number =  3;
+  public consomationTotale : number =  this.distanceKm * this.consommationPour100Km;
+  public voyages = [
+    { distanceKm: 50, consommationPour100Km: 5 },
+    { distanceKm: 150, consommationPour100Km: 6 },
+    { distanceKm: 250, consommationPour100Km: 7 },
+    { distanceKm: 350, consommationPour100Km: 8 },
+    { distanceKm: 450, consommationPour100Km: 9 }
+  ]
 
   constructor() {
     console.log('Composant instancié');
@@ -31,4 +45,7 @@ export class CarbonFootprintComponent {
     console.log('Le composant est détruit !')
   }
 
+  ajouter100KM() {
+    this.distanceKm+= 100;
+  }
 }
